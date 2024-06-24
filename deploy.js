151b1724ea3fdda98e74ee0f178694f2a8ad6a78
folza1 <fodorzoltan317@gmail.com>
2024-06-24 22:27:23 +0200

    FTP set

---
+++ deploy.js
@@ -0,0 +1,18 @@
+const FtpDeploy = require('ftp-deploy');
+const ftpDeploy = new FtpDeploy();
+
+const config = {
+  user: process.env.FTP_USERNAME,
+  password: process.env.FTP_PASSWORD,
+  host: process.env.FTP_HOST,
+  port: 21,
+  localRoot: __dirname + '/dist',
+  remoteRoot: '/htdocs/', // állítsd be a szerver könyvtárát
+  include: ['*', '**/*'], // minden fájlt másol
+  deleteRemote: false, // törli az összes távoli fájlt
+  forcePasv: true
+};
+
+ftpDeploy.deploy(config)
+  .then(res => console.log('Deploy finished:', res))
+  .catch(err => console.log('Deploy error:', err));
